4a3a37fc03892507b9cd6061ab993928ed121233
Sharmin Sultana <sharmin9518@gmail.com>
2024-08-03 15:06:36 +0600

    desingn added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { brainwave } from "../assets/assets/";
 import { navigation } from "../constants";
+import Button from "./Button";
+import { HamburgerMenu } from "../components/design/Header";
+
+import { useState } from "react";
 
 const Header = () => {
+  const pathname = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(false);
+
   return (
-    <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-small border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur=sm">
+    <div
+      className={`fixed top-0 z-50 left-0 w-full bg-n-8/90 backdrop-blur-small border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+      }`}
+    >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <Link to="/" className="block w-(12rem) xl:mr=8">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
@@ -12,12 +24,27 @@ const Header = () => {
         <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
-              <Link className="m-1" to={item?.url} key={item.id}>
+              <Link
+                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+                  item.onlyMobile ? "lg:hidden" : ""
+                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                  item.url === pathname.hash ? "z-2 lg:text-n-1" : "lg:text-n-1/50"
+                }lg:leading-5 lg:hover:text-n-1 xl:px-12 `}
+                to={item?.url}
+                key={item.id}
+              >
                 {item.title}
               </Link>
             ))}
           </div>
         </nav>
+        <Link href="#signup" className="button-hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:black">
+          New account
+        </Link>
+        <Button className="hidden lg:flex" href="#login">
+          Sign in
+        </Button>
+        <HamburgerMenu />
       </div>
     </div>
   );
