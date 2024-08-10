679e24cb36f62a48e3ad61a582a7125487719610
Sharmin Sultana <sharmin9518@gmail.com>
2024-08-10 21:08:46 +0600

    error found in hero secton

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,26 @@
 import ButtonSvg from "../assets/assets/svg/ButtonSvg";
 
 const Button = ({ className, href, onClick, children, px, white }) => {
-  const classess = `button relative infline-flex items-center justify-center h-11 transition-colors hover:text-color-1 $ {px || "px-7"} ${
-    white ? "text-n-*" : "text-n-1"
-  } $ {className || ""}`;
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || "px-7"} ${
+    white ? "text-n-8" : "text-n-1"
+  } ${className || ""}`;
+  const spanClasses = "relative z-10";
+
   const renderButton = () => (
-    <button className={classess} onClick={onClick}>
-      <span>{children}</span>
+    <button className={classes} onClick={onClick}>
+      <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
   );
-  return renderButton();
+
+  const renderLink = () => (
+    <a href={href} className={classes} target="_blank" rel="noopener noreferrer">
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </a>
+  );
+
+  return href ? renderLink() : renderButton();
 };
 
 export default Button;
